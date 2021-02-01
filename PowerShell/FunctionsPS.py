bbe554646961f3e180ef347b5bf247deec44b8e1
Thai Son <thaisonqng@gmail.com>
2021-02-01 15:16:21 +0700

    remove function

--- PowerShell/FunctionsPS.py
+++ PowerShell/FunctionsPS.py
@@ -17,38 +17,19 @@ class FunctionsPS:
             shutil.rmtree(to_path)
         shutil.copytree(from_path, to_path)
 
-    def RunPowerShellScript(self  ,  function, param ):
+    def RunPowerShellScript(self,  function, param ):
         commandline_options = 'PowerShell -noprofile -ex unrestricted -Command "& { . ' + self.PsScripFile + ' ; ' + function  + ' ' + param + ' }"'
         print("commandline_options : ",commandline_options)
         process_result = subprocess.run(commandline_options, stdout=subprocess.PIPE, stderr=subprocess.PIPE,  universal_newlines=True)
-        print("returncode  0 = SUCCESS, NON-ZERO = FAIL: ",process_result.returncode)  # PRINT RETURN CODE OF PROCESS  0 = SUCCESS, NON-ZERO = FAIL
-        print("OUTPUT FROM POWERSHELL : ",process_result.stdout)
+        # PRINT RETURN CODE OF PROCESS  0 = SUCCESS, NON-ZERO = FAIL
         if process_result.returncode == 0:  # COMPARING RESULT
             return process_result.stdout
         else:
             return process_result.stderr
 
-    def GetADComputerToJson(self,like):
-        functionPS = "GetADComputerToJson"
-        param = like
-        commandline_options = 'PowerShell -noprofile -ex unrestricted -Command "& { . ' + self.PsScripFile + ' ; ' + functionPS + ' ' + param + ' }"'
-        print("commandline_options:", commandline_options)
-        subprocess.call(commandline_options, shell=True)  # , stdout=subprocess.PIPE, stderr=subprocess.STDOUT )
-
     def GetRSJobToLogFile(self,param):
         functionPS = "GetADComputerToJson"
         commandline_options = 'PowerShell -noprofile -ex unrestricted -Command "& { . ' + self.PsScripFile + ' ; ' + functionPS + ' ' + param + ' }"'
-        subprocess.call(commandline_options, shell=True)
-    def GetSoftwareInstalled(self,computers):
-        param="-Computers "
-        i=0
-        while i < len(computers) -1 :
-            param += computers[i] + "0"
-            i += 1
-        param += computers[len(computers)-1]
-        # commandline_options = 'PowerShell -noprofile -ex unrestricted -Command "& { . ' + self.PsScripFile + ' ; ' + "GetSoftwareInstalled" + ' ' + param + ' }"'
-        commandline_options = 'PowerShell -noprofile -ex unrestricted -Command "& { . ' + self.GetRemoteProgram + ' ; ' + "GetRemoteProgram" + ' ' + param + ' }"'
-
         subprocess.call(commandline_options, shell=True)
 
     def RemoveSoftware(self,computerName, software):
@@ -56,23 +37,10 @@ class FunctionsPS:
         commandline_options = 'PowerShell -noprofile -ex unrestricted -Command "& { . ' + self.PsScripFile + ' ; ' + "RemoveSoftware" + ' ' + computerName+","+software + ' }"'
         print("commandline_options:",commandline_options)
         subprocess.call(commandline_options, shell=True)
+
     def RemoveSoftwareUninstallString(self,computerName, software):
         pscommand = "{Invoke-Command -ComputerName  " + computerName + " { Start-Process -FilePath  '" + software + "'   -ArgumentList '/S' -Wait }}"
         commandline_options = ' PowerShell -noprofile -ex unrestricted -Command "& { . ' + pscommand + '"  }"'
         print("commandline_options:", commandline_options)
         subprocess.call(commandline_options, shell=True)
-
-
-    def RunSubprocess(self):
-        command = 'PowerShell -noprofile -ex unrestricted -Command "$env:USERPROFILE"'
-        process = subprocess.Popen( (command), stdout=subprocess.PIPE)
-        while True:
-            output = process.stdout.readline()
-            if output == '' and process.poll() is not None:
-                break
-            if output:
-                print
-                output.strip()
-        rc = process.poll()
-        return rc
 
